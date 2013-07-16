c0e000445a8d05b3dddbf3100ba4e724bd872c57
Stryker <sjstryker33@gmail.com>
2013-07-16 15:01:29 +0530

    160713_1501

--- mysql-cluster-data/scripts/f1_driver_db.sql
+++ mysql-cluster-data/scripts/f1_driver_db.sql
@@ -1,51 +1,51 @@
 
 Insert into user (profile_image, emailid, password, first_name, last_name, dob, gender, current_home, hometown) values 
-("https://localhost/images/profile_images/1_pi.jpg", "fernando_alonso@gmail.com", "fernandoA", "Fernando", "Alonso", "29 July, 1981", "M", "Montreal, Quebec, CA", "Barcelona, Catalonia, ES");
+("https://localhost/images/profile_images/1_pi.jpg", "fernando_alonso@gmail.com", "0454fcdc53d1b48c2c98d7a53b6755e95b9878fe", "Fernando", "Alonso", "29 July, 1981", "M", "Montreal, Quebec, CA", "Barcelona, Catalonia, ES");
 Insert into edu_desc(uid, institute_name) values (1, "University of Catalunya");
 Insert into work_desc(uid, work_designation, work_place_name) values (1, "Lead Driver", "Ferrari Formula 1 Team");
 
 Insert into user (profile_image, emailid, password, first_name, last_name, dob, gender, current_home, hometown) values 
-("https://localhost/images/profile_images/2_pi.jpg", "kimi_raikonnen@gmail.com", "kimiR", "Kimi", "Raikonnen", "17 October, 1979", "M", "Montreal, Quebec, CA", "Espoo, Southern Finland Province, FI");
+("https://localhost/images/profile_images/2_pi.jpg", "kimi_raikonnen@gmail.com", "80591682db6395b16256be4819a22afba4da063c", "Kimi", "Raikonnen", "17 October, 1979", "M", "Montreal, Quebec, CA", "Espoo, Southern Finland Province, FI");
 Insert into edu_desc(uid, institute_name) values (2, "University of Espoo");
 Insert into work_desc(uid, work_designation, work_place_name) values (2, "Lead Driver", "Lotus Formula 1 Team");
 
 Insert into user (profile_image, emailid, password, first_name, last_name, dob, gender, current_home, hometown) values 
-("https://localhost/images/profile_images/3_pi.jpg", "seb_vettel@gmail.com", "sebV", "Sebastian", "Vettel", "3 July, 1987", "M", "Montreal, Quebec, CA", "Heppenheim, Hesse, DE");
+("https://localhost/images/profile_images/3_pi.jpg", "seb_vettel@gmail.com", "0d00f34050063a30a06e70507d8bb5457e8f49da", "Sebastian", "Vettel", "3 July, 1987", "M", "Montreal, Quebec, CA", "Heppenheim, Hesse, DE");
 Insert into edu_desc(uid, institute_name) values (3, "University of Hesse");
 Insert into work_desc(uid, work_designation, work_place_name) values (3, "Lead Driver", "Red Bull Formula 1 Team");
 
 Insert into user (profile_image, emailid, password, first_name, last_name, dob, gender, current_home, hometown) values 
-("https://localhost/images/profile_images/4_pi.jpg", "lewis_hamilton@gmail.com", "lewisH", "Lewis", "Hamilton", "7 January, 1985", "M", "Montreal, Quebec, CA", "Stevenage, England, GB");
+("https://localhost/images/profile_images/4_pi.jpg", "lewis_hamilton@gmail.com", "b2203f6c3fae74a357e13b683b65688fb2e85352", "Lewis", "Hamilton", "7 January, 1985", "M", "Montreal, Quebec, CA", "Stevenage, England, GB");
 Insert into edu_desc(uid, institute_name) values (4, "University of Stevenage");
 Insert into work_desc(uid, work_designation, work_place_name) values (4, "Driver", "Mercedes Formula 1 Team");
 
 Insert into user (profile_image, emailid, password, first_name, last_name, dob, gender, current_home, hometown) values 
-("https://localhost/images/profile_images/5_pi.jpg", "mark_webber@gmail.com", "markW", "Mark", "Webber", "27 August, 1976", "M", "Montreal, Quebec, CA", "Queanbeyan, New South Wales, AU");
+("https://localhost/images/profile_images/5_pi.jpg", "mark_webber@gmail.com", "5067fb17191b8e59f79a4fb9b1464b5ab536bdce", "Mark", "Webber", "27 August, 1976", "M", "Montreal, Quebec, CA", "Queanbeyan, New South Wales, AU");
 Insert into edu_desc(uid, institute_name) values (5, "University of Queanbeyan");
 Insert into work_desc(uid, work_designation, work_place_name) values (5, "Driver", "Red Bull Formula 1 Team");
 
 Insert into user (profile_image, emailid, password, first_name, last_name, dob, gender, current_home, hometown) values 
-("https://localhost/images/profile_images/6_pi.jpg", "nico_rosberg@gmail.com", "nicoR", "Nico", "Rosberg", "27 June, 1985", "M", "Montreal, Quebec, CA", "Wiesbaden, Hesse, DE");
+("https://localhost/images/profile_images/6_pi.jpg", "nico_rosberg@gmail.com", "dad6af2a3644e41696fed5d79d6310d12ce2523c", "Nico", "Rosberg", "27 June, 1985", "M", "Montreal, Quebec, CA", "Wiesbaden, Hesse, DE");
 Insert into edu_desc(uid, institute_name) values (6, "University of Hesse");
 Insert into work_desc(uid, work_designation, work_place_name) values (6, "Lead Driver", "Mercedes Formula 1 Team");
 
 Insert into user (profile_image, emailid, password, first_name, last_name, dob, gender, current_home, hometown) values 
-("https://localhost/images/profile_images/7_pi.jpg", "felipe_massa@gmail.com", "felipeM", "Felipe", "Massa", "25 April, 1981", "M", "Montreal, Quebec, CA", "Sao Paolo, BR");
+("https://localhost/images/profile_images/7_pi.jpg", "felipe_massa@gmail.com", "942d6fdf66f4b41cc4dce5f2c291acf01e7d32f8", "Felipe", "Massa", "25 April, 1981", "M", "Montreal, Quebec, CA", "Sao Paolo, BR");
 Insert into edu_desc(uid, institute_name) values (7, "University of Sao Paolo");
 Insert into work_desc(uid, work_designation, work_place_name) values (7, "Driver", "Ferrari Formula 1 Team");
 
 Insert into user (profile_image, emailid, password, first_name, last_name, dob, gender, current_home, hometown) values 
-("https://localhost/images/profile_images/8_pi.jpg", "jenson_button@gmail.com", "jensonB", "Jenson", "Button", "19 January, 1980", "M", "Montreal, Quebec, CA", "Frome, England, UK");
+("https://localhost/images/profile_images/8_pi.jpg", "jenson_button@gmail.com", "c36c888489aa7c28f6f85c84421e44d8bed2e35b", "Jenson", "Button", "19 January, 1980", "M", "Montreal, Quebec, CA", "Frome, England, UK");
 Insert into edu_desc(uid, institute_name) values (8, "University of Frome");
 Insert into work_desc(uid, work_designation, work_place_name) values (8, "Lead Driver", "McLaren Formula 1 Team");
 
 Insert into user (profile_image, emailid, password, first_name, last_name, dob, gender, current_home, hometown) values 
-("https://localhost/images/profile_images/9_pi.jpg", "paul_di_resta@gmail.com", "paulR", "Paul", "Di Resta", "16 April, 1986", "M", "Montreal, Quebec, CA", "Uphall, Scotland, UK");
+("https://localhost/images/profile_images/9_pi.jpg", "paul_di_resta@gmail.com", "a93b6dadfaefbaf13ff1c5baac5c928324f21ee2", "Paul", "Di Resta", "16 April, 1986", "M", "Montreal, Quebec, CA", "Uphall, Scotland, UK");
 Insert into edu_desc(uid, institute_name) values (9, "University of Uphall");
 Insert into work_desc(uid, work_designation, work_place_name) values (9, "Lead Driver", "Force India Formula 1 Team");
 
 
 Insert into user (profile_image, emailid, password, first_name, last_name, dob, gender, current_home, hometown) values 
-("https://localhost/images/profile_images/10_pi.jpg", "romain_grosjean@gmail.com", "romainG", "Romain", "Grosjean", "17 April, 1986", "M", "Montreal, Quebec, CA", "Geneva, CH");
+("https://localhost/images/profile_images/10_pi.jpg", "romain_grosjean@gmail.com", "0454fcdc53d1b48c2c98d7a53b6755e95b9878fe", "Romain", "Grosjean", "17 April, 1986", "M", "Montreal, Quebec, CA", "Geneva, CH");
 Insert into edu_desc(uid, institute_name) values (10, "University of Geneva");
 Insert into work_desc(uid, work_designation, work_place_name) values (10, "Driver", "Lotus Formula 1 Team");
